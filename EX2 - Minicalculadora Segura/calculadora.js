df1e775ee5150f9b29f4848ab91423cd839be67c
danilodev92 <dortiz@blacksmithresearch.onmicrosoft.com>
2026-09-01 19:01:24 -0500

    Inicio de taller y ejercicio 2 de la calculadora segura

---
+++ EX2 - Minicalculadora Segura/calculadora.js
@@ -0,0 +1,7 @@
+let numero1 prompt("introducir numero 1: ");
+let numero2 prompt("intruducir numero 2: ");
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
